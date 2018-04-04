36dc13de2715db727b2ae9afa64004ec04f8b0dd
andreistalker <usadekall@gmail.com>
2018-04-04 19:42:50 +0300

    Roulette animations are ready.
    We have implemented the roulette animation, we only need now just the brain of the roulette.

--- htdocs/js/roulette.js
+++ htdocs/js/roulette.js
@@ -1,3 +1,192 @@
-function createGame() {
+function getUnder() {
+	var currentUrl = window.location.href;
+	var under = 0;
+	notPhp = currentUrl.indexOf("#");
 	
+	if(notPhp != -1) {
+		currentUrl = currentUrl.substring(currentUrl.indexOf("#")+1);
+		
+		if(currentUrl > 36 && currentUrl != 0 || currentUrl == "" || currentUrl == "00")
+			under = 0;
+		else if(currentUrl == "zz")
+			under = 37;
+		else
+			under = currentUrl;
+	} else 
+		under = 0;
+	
+	return under;
+}
+
+function calculateZero() {
+	var under = getUnder();
+	var returned;
+	
+	if(under == 0)
+		returned = 0;
+	else if(under == 1)
+		returned = 38;
+	else if(under == 13)
+		returned = 37;
+	else if(under == 36)
+		returned = 36;
+	else if(under == 24)
+		returned = 35;
+	else if(under == 3)
+		returned = 34;
+	else if(under == 15)
+		returned = 33;
+	else if(under == 34)
+		returned = 32;
+	else if(under == 22)
+		returned = 31;
+	else if(under == 5)
+		returned = 30;
+	else if(under == 17)
+		returned = 29;
+	else if(under == 32)
+		returned = 28;
+	else if(under == 20)
+		returned = 27;
+	else if(under == 7)
+		returned = 26;
+	else if(under == 11)
+		returned = 25;
+	else if(under == 30)
+		returned = 24;
+	else if(under == 26)
+		returned = 23;
+	else if(under == 9)
+		returned = 22;
+	else if(under == 28)
+		returned = 21;
+	else if(under == 37)
+		returned = 20;
+	else if(under == 2)
+		returned = 19;
+	else if(under == 14)
+		returned = 18;
+	else if(under == 35)
+		returned = 17;
+	else if(under == 23)
+		returned = 16;
+	else if(under == 4)
+		returned = 15;
+	else if(under == 16)
+		returned = 14;
+	else if(under == 33)
+		returned = 13;
+	else if(under == 21)
+		returned = 12;
+	else if(under == 6)
+		returned = 11;
+	else if(under == 18)
+		returned = 10;
+	else if(under == 31)
+		returned = 9;
+	else if(under == 19)
+		returned = 8;
+	else if(under == 8)
+		returned = 7;
+	else if(under == 12)
+		returned = 6;
+	else if(under == 29)
+		returned = 5;
+	else if(under == 25)
+		returned = 4;
+	else if(under == 10)
+		returned = 3;
+	else if(under == 27)
+		returned = 2;
+	
+	return returned;
+}
+
+function playAnimation(onWath) {
+	var moves = calculateZero();
+	moves = moves + 38;
+	
+	var howMuch;
+	
+	if(onWath == 0)
+		howMuch = 0;
+	else if(onWath == 1)
+		howMuch = 1;
+	else if(onWath == 13)
+		howMuch = 2;
+	else if(onWath == 36)
+		howMuch = 3;
+	else if(onWath == 24)
+		howMuch = 4;
+	else if(onWath == 3)
+		howMuch = 5;
+	else if(onWath == 15)
+		howMuch = 6;
+	else if(onWath == 34)
+		howMuch = 7;
+	else if(onWath == 22)
+		howMuch = 8;
+	else if(onWath == 5)
+		howMuch = 9;
+	else if(onWath == 17)
+		howMuch = 10;
+	else if(onWath == 32)
+		howMuch = 11;
+	else if(onWath == 20)
+		howMuch = 12;
+	else if(onWath == 7)
+		howMuch = 13;
+	else if(onWath == 11)
+		howMuch = 14;
+	else if(onWath == 30)
+		howMuch = 15;
+	else if(onWath == 26)
+		howMuch = 16;
+	else if(onWath == 9)
+		howMuch = 17;
+	else if(onWath == 28)
+		howMuch = 18;
+	else if(onWath == 37)
+		howMuch = 19;
+	else if(onWath == 2)
+		howMuch = 20;
+	else if(onWath == 14)
+		howMuch = 21;
+	else if(onWath == 35)
+		howMuch = 22;
+	else if(onWath == 23)
+		howMuch = 23;
+	else if(onWath == 4)
+		howMuch = 24;
+	else if(onWath == 16)
+		howMuch = 25;
+	else if(onWath == 33)
+		howMuch = 26;
+	else if(onWath == 21)
+		howMuch = 27;
+	else if(onWath == 6)
+		howMuch = 28;
+	else if(onWath == 18)
+		howMuch = 29;
+	else if(onWath == 31)
+		howMuch = 30;
+	else if(onWath == 19)
+		howMuch = 31;
+	else if(onWath == 8)
+		howMuch = 32;
+	else if(onWath == 12)
+		howMuch = 33;
+	else if(onWath == 29)
+		howMuch = 34;
+	else if(onWath == 25)
+		howMuch = 35;
+	else if(onWath == 10)
+		howMuch = 36;
+	else if(onWath == 27)
+		howMuch = 37;
+	
+	moves = moves + howMuch;
+	
+	i = 0;
+	roll(moves);
 }
--- htdocs/roulette.php
+++ htdocs/roulette.php
@@ -9,65 +9,69 @@ include_once('src/db.php');
 		<?php include('src/head.php'); ?>
 		<meta http-equiv="refresh" content="<?php echo $secrefresh?>;URL='<?php echo $page?><?php if($past == 1) echo "?past=1";?>'">
 		<script src="dist/owl.carousel.min.js"></script>
-		<script src="js/roulette.js"></script>
 	</head>
 	<body>
 		<?php include('navbar.php'); ?>
 		<div class="roulette-body">
 			<center><h1 style="display:inline">Roulette </h1><b><a href="games.php" style="display:inline;text-decoration:none;color:black;">(Go back)</a></b>
-			<div class="owl-carousel">
-			  <div data-hash="0" > 0 </div>
-			  <div data-hash="1" > 1 </div>
-			  <div data-hash="13" > 13 </div>
-			  <div data-hash="36" > 36 </div>
-			  <div data-hash="24" > 24 </div>
-			  <div data-hash="3" > 3 </div>
-			  <div data-hash="15" > 15 </div>
-			  <div data-hash="34" > 34 </div>
-			  <div data-hash="22" > 22 </div>
-			  <div data-hash="5" > 5 </div>
-			  <div data-hash="17" > 17 </div>
-			  <div data-hash="32" > 32 </div>
-			  <div data-hash="20" > 20 </div>
-			  <div data-hash="7" > 7 </div>
-			  <div data-hash="11" > 11 </div>
-			  <div data-hash="30" > 30 </div>
-			  <div data-hash="26" > 26 </div>
-			  <div data-hash="9" > 9 </div>
-			  <div data-hash="28" > 28 </div>
-			  <div data-hash="00" > 00 </div>
-			  <div data-hash="2" > 2 </div>
-			  <div data-hash="14" > 14 </div>
-			  <div data-hash="35" > 35 </div>
-			  <div data-hash="23" > 23 </div>
-			  <div data-hash="4" > 4 </div>
-			  <div data-hash="16" > 16 </div>
-			  <div data-hash="33" > 33 </div>
-			  <div data-hash="21" > 21 </div>
-			  <div data-hash="6" > 6 </div>
-			  <div data-hash="18" > 18 </div>
-			  <div data-hash="31" > 31 </div>
-			  <div data-hash="19" > 19 </div>
-			  <div data-hash="8" > 8 </div>
-			  <div data-hash="12" > 12 </div>
-			  <div data-hash="29" > 29 </div>
-			  <div data-hash="25" > 25 </div>
-			  <div data-hash="10" > 10 </div>
-			  <div data-hash="27" > 27 </div>
+			<div class="roulette-box">
+				<div class="roulette-sign"></div>
+				<div class="owl-carousel">
+				  <div class="roulette" id="green" data-hash="0" > 0 </div>
+				  <div class="roulette" id="red" data-hash="1" > 1 </div>
+				  <div class="roulette" id="black" data-hash="13" > 13 </div>
+				  <div class="roulette" id="red" data-hash="36" > 36 </div>
+				  <div class="roulette" id="black" data-hash="24" > 24 </div>
+				  <div class="roulette" id="red" data-hash="3" > 3 </div>
+				  <div class="roulette" id="black" data-hash="15" > 15 </div>
+				  <div class="roulette" id="red" data-hash="34" > 34 </div>
+				  <div class="roulette" id="black" data-hash="22" > 22 </div>
+				  <div class="roulette" id="red" data-hash="5" > 5 </div>
+				  <div class="roulette" id="black" data-hash="17" > 17 </div>
+				  <div class="roulette" id="red" data-hash="32" > 32 </div>
+				  <div class="roulette" id="black" data-hash="20" > 20 </div>
+				  <div class="roulette" id="red" data-hash="7" > 7 </div>
+				  <div class="roulette" id="black" data-hash="11" > 11 </div>
+				  <div class="roulette" id="red" data-hash="30" > 30 </div>
+				  <div class="roulette" id="black" data-hash="26" > 26 </div>
+				  <div class="roulette" id="red" data-hash="9" > 9 </div>
+				  <div class="roulette" id="black" data-hash="28" > 28 </div>
+				  <div class="roulette" id="green" data-hash="zz" > 00 </div>
+				  <div class="roulette" id="black" data-hash="2" > 2 </div>
+				  <div class="roulette" id="red" data-hash="14" > 14 </div>
+				  <div class="roulette" id="black" data-hash="35" > 35 </div>
+				  <div class="roulette" id="red" data-hash="23" > 23 </div>
+				  <div class="roulette" id="black" data-hash="4" > 4 </div>
+				  <div class="roulette" id="red" data-hash="16" > 16 </div>
+				  <div class="roulette" id="black" data-hash="33" > 33 </div>
+				  <div class="roulette" id="red" data-hash="21" > 21 </div>
+				  <div class="roulette" id="black" data-hash="6" > 6 </div>
+				  <div class="roulette" id="red" data-hash="18" > 18 </div>
+				  <div class="roulette" id="black" data-hash="31" > 31 </div>
+				  <div class="roulette" id="red" data-hash="19" > 19 </div>
+				  <div class="roulette" id="black" data-hash="8" > 8 </div>
+				  <div class="roulette" id="red" data-hash="12" > 12 </div>
+				  <div class="roulette" id="black" data-hash="29" > 29 </div>
+				  <div class="roulette" id="red" data-hash="25" > 25 </div>
+				  <div class="roulette" id="black" data-hash="10" > 10 </div>
+				  <div class="roulette" id="red" data-hash="27" > 27 </div>
+				</div>
 			</div>
 		</div>
-		
-		<a href="#" onclick="createGame()">Create game</a>
 		<script>
+			var owl, i;
 			$(document).ready(function() {
  
-				var owl = $('.owl-carousel');
+				owl = $('.owl-carousel');
 				owl.owlCarousel({
 					loop:true,
-					margin:10,
+					margin:0,
 					nav:false,
 					URLhashListener:true,
 					startPosition: 'URLHash',
+					touchDrag: false,
+					mouseDrag: false,
+					autoWidth:true,
 					center: true,
 					responsive:{
 						0:{
@@ -80,20 +84,17 @@ include_once('src/db.php');
 							items:7
 						}
 					}
-				});
-				
-				var i;
-				
-				function roll(howMuch) {
-					i++;
-					if(i < howMuch) {
-						owl.trigger('next.owl.carousel', [200]);
-						setTimeout(function() {roll();}, 150);
-					}
-				}
- 
+				}); 
 			});
+			function roll(howMuch) {
+				i++;
+				if(i < howMuch) {
+					owl.trigger('next.owl.carousel', [100]);
+					setTimeout(function() {roll(howMuch);}, 80);
+				}
+			}
 		</script>
+		<script src="js/roulette.js"></script>
 		<?php include('src/footer.php'); ?>
 	</body>
 </html>
