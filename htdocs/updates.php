b149745496e0eca5383cbfcdcadb4d455386d790
andreistalker <usadekall@gmail.com>
2018-05-28 21:47:27 +0300

    Updated the updates page

--- htdocs/updates.php
+++ htdocs/updates.php
@@ -7,9 +7,19 @@
 		<div>
 			<center>
 				<h1 style="margin-bot:0">Updates</h1>
+				<h3 style="margin-top:0">(24 of May 2018)</h3>
+				<p>
+					- Added new custom bets to roulette.<br>
+				</p>
+				<h3 style="margin-top:0">(17 of May 2018)</h3>
+				<p>
+					- Changed roulette time to 30 seconds.<br>
+					- Fixed a bug where you could withdraw/bet negative amounts.<br>
+				</p>
 				<h3 style="margin-top:0">(1 of May 2018)</h3>
 				<p>
 					- Added your last 3 rolls to Dices.<br>
+					- Mines balance now updates after a win or after you start a game.<br>
 				</p>
 				<h3 style="margin-top:0">(29 of April 2018)</h3>
 				<p>
